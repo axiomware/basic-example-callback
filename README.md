b9f371cc2ba637a19feda109b6eb7d4570c5a270
a-gomes <goalgov@gmail.com>
2018-02-19 13:08:12 -0800

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # basic-example-callback
-Basic example of using netrunr-gapi JS SDK
+Basic Nodejs example of using [Axiomware's](http://www.axiomware.com) [netrunr-gapi](http://www.axiomware.com/apidocs/index.html) Javascript SDK
+
+Program to illustrate Netrunr API functions. The program will perform the following functions: 1) connect to your account, 2) list all gateways associated with this account, 3) connect to the first gateway in the list, 4) open a network connection, 5) get version information from the gateway, and 6) exit program by closing connections and loging out of your account.
+
+## SDK, Documentation and examples
+- [Netrunr B24C API Doucmentation](http://www.axiomware.com/apidocs/index.html)
+- [Netrunr-gapi SDK](https://github.com/axiomware/netrunr-gapi-js)
+  - [List of Netrunr-gapi examples](https://github.com/axiomware/list-of-examples-netrunr-gapi)
+- [Netrunr-gapi-async SDK](https://github.com/axiomware/netrunr-gapi-async-js)
+  - [List of Netrunr-gapi-async examples](https://github.com/axiomware/list-of-examples-netrunr-gapi-async)
+
+
+## Requirements
+
+- [Netrunr B24C](http://www.axiomware.com/netrunr-b24c-product.html) gateway
+- Axiomware cloud account. See the Netrunr [quick start guide](http://www.axiomware.com/page-netrunr-b24c-qs-guide.html) on creating an account.
+- Nodejs (see [https://nodejs.org/en/](https://nodejs.org/en/) for download and installation instructions)
+- NPM (Node package manager - part of Nodejs)   
+- Windows, MacOS or Linux computer with access to internet
+
+## Installation
+
+Clone the repo
+
+`git clone https://github.com/axiomware/basic-example-callback.git`
+
+or download as zip file to a local directory and unzip.
+
+Install all module dependencies by running the following command inside the directory
+
+  `npm install`
+
+## Customization required before running the program
+The basic version of program has no User Interface(UI) to simplify the code.  Credentials to your Axiomware account are hardcoded inside the program. Please modify the following two lines in `appLogin.js` with valid username and password:
+```javascript
+//User configuration
+var userConfig = {           
+    'username': '<ENTER-YOUR-USERNAME>', //Valid username to your Axiomware account
+    'pwd': '<ENTER-YOUR-PASSWORD>' //Valid password to your Axiomware account
+};
+```
+
+## Usage
+
+Run the nodejs application:
+
+    node appLogin.js
+
+The program will exit after connecting to the gateway. To force exit, use:
+
+    CTRL-C  
+
+## Error conditions
+
+- If the program is not able to login, check your credentials.
+- If the gateway is not listed in your account, it may not have been successfully provisioned. See the Netrunr [quick start guide](http://www.axiomware.com/page-netrunr-b24c-qs-guide.html) for provisioning the gateway.
+- Not able to get version information of the gateway. Check if gateway is powered ON and has access to internet. Also, check if firewall is blocking internet access.
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
---
+++ appLogin.js
@@ -0,0 +1,110 @@
+// Copyright 2017,2018 Axiomware Systems Inc. 
+//
+// Licensed under the MIT license <LICENSE-MIT or 
+// http://opensource.org/licenses/MIT>. This file may not be copied, 
+// modified, or distributed except according to those terms.
+//
+
+//Add external modules dependencies
+var netrunr = require('netrunr-gapi');
+var chalk = require('chalk');
+var figlet = require('figlet');
+
+var gapi = new netrunr.gapi();  //Create a Netrunr gateway instance(only for login)
+
+//User configuration
+var userConfig = {           
+    'username': '<ENTER-YOUR-USERNAME>', //Valid username to your Axiomware account
+    'pwd': '<ENTER-YOUR-PASSWORD>' //Valid password to your Axiomware account
+};
+
+//Monitor ctrl-c events and exit program
+process.on("SIGINT", function () {
+    axShutdown("Received Ctrl-C - shutting down.. please wait");
+});
+
+//Application start - startup banner
+console.log(chalk.green.bold(figlet.textSync('NETRUNR GATEWAY', { horizontalLayout: 'default' })));
+console.log(chalk.green.bold('Basic Login Application Using Callbacks'));
+console.log(chalk.red.bold('Press Ctrl-C to exit'));
+axLogin(userConfig.username, userConfig.pwd); // Call the main function
+
+/**
+ * Main program entry point
+ * 
+ * @param {string} user - username of axiomware account
+ * @param {string} pwd - password of axiomware account
+ */
+function axLogin(user , pwd) {
+    gapi.login({ 'user': user, 'pwd': pwd },//login into your account
+        function (robj) {
+            console.log('Login success [user:' + user + ']');
+            if (robj.gwid.length > 0) {
+                console.log('Found ' + robj.gwid.length + ' Gateway(s)');
+                robj.gwid.forEach(function (gw) { console.log(gw) }); // print gateway list
+                axOpenConnection(robj.gwid[0]);//open connection to the first gateway in the list
+            }
+            else {// no gateways fount. Exit
+                axShutdown('Found no gateways - exiting (nothing to do)');
+            }
+        },
+        function (robj) {
+            axShutdown('Login error!\nCheck the Javascript program file.\nYou need to enter valid credentails for \"username\" and \"pwd\" variables.\nExiting ...');
+        });
+}
+
+/**
+ * Open a connection to a specific Netrunr gateway
+ * 
+ * @param {string} gwid - gateway ID
+ */
+function axOpenConnection(gwid) {
+    gapi.config({ 'gwid': gwid }); //select gateway
+
+    gapi.open({},//open connection to the gateway
+        function (robj) {
+            console.log('Connection open to Netrunr gateway [' + gwid + '] success!')
+            axGetVersionInfo(gwid);//Get version information
+        },
+        function (robj) {
+            axShutdown('Failed to open connection - exiting');
+        }
+    );
+};
+
+/**
+ * Get version information from the Netrunr gateway
+ * 
+ *  @param {string} gwid - gateway ID
+ */
+function axGetVersionInfo(gwid) {
+    console.log('Fetching version info of [gwid:' + gwid + ']');
+    gapi.version({},//get gateway version
+        function (robj) {
+            axShutdown('Netrunr gateway [' + gwid + '] version = ' + robj.version + '\nAll Done! Exiting program...');//All done.. exit program
+        },
+        function (robj) {
+            axShutdown('Failed to open connection - exiting');
+        }
+    );
+};
+
+//let ver = await gapiAsync.version(5000);                              //Check gateway version - if gateway is not online(err), exit 
+
+/**
+ * Gracefully shutdown the connection and logout of the account
+ * 
+ * @param {string} prnStr - Print this string to console before exiting
+ */
+function axShutdown(prnStr) {
+    console.log(prnStr);
+    gapi.close({}, gapi.logout({}, axExit, axExit), gapi.logout({}, axExit, axExit));//close and logout
+};
+
+/**
+ * Exit the process
+ * 
+ */
+function axExit() {
+    process.exit();//exit the process
+}
